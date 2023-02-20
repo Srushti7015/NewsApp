da5db57685ddfe4529bd4b9bab4529a08e5b4e55
Srushti7015 <123812482+Srushti7015@users.noreply.github.com>
2023-02-20 11:35:10 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BusinessNewsComponent } from './business-news/business-news.component';
+import { CrimeComponent } from './crime/crime.component';
+import { HeaderComponent } from './header/header.component';
+import { PoliticsComponent } from './politics/politics.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
+import { WorldComponent } from './world/world.component';
+import { DescriptionPipe } from './description-pipe.pipe';
+
+
+const allLinks:Routes = [
+  {path:'BusinessNews',component:BusinessNewsComponent},
+  {path:'Politics',component:PoliticsComponent},
+  {path:'Crime',component:CrimeComponent},
+  {path:'World',component:WorldComponent},
+  {path:'Technology',component:TechnologyComponent},
+  {path:'Sports',component:SportsComponent},
+  {path:'newsDetail/:category',component:ViewDetailsComponent},
+  
+  
+  
+  
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BusinessNewsComponent,
+    CrimeComponent,
+    HeaderComponent,
+    PoliticsComponent,
+    SportsComponent,
+    TechnologyComponent,
+    ViewDetailsComponent,
+    WorldComponent,
+    DescriptionPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/description-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DescriptionPipe } from './description-pipe.pipe';
+
+describe('DescriptionPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DescriptionPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
